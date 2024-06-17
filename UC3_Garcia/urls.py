35dbb26bd139111af7ab809ce5c45cba4d4441fb
AbrahamGCh <1823110013@untels.edu.pe>
2024-06-17 16:56:25 -0500

    UC3_Parte02_Garcia

--- UC3_Garcia/urls.py
+++ UC3_Garcia/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from miapp import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("inicio/",views.inicio,name="saludo"),
+    path("",views.inicio,name="inicio")
 ]
--- miapp/views.py
+++ miapp/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def inicio(request):
+    mensaje = """ <h1> Bienvenido a LP3 - GARCIA </h1>"""
+    return HttpResponse(mensaje)
